bca556087618d2d55e74f45cacca495a6d11c67a
BurntRobot <Bydan14@yandex.ru>
2023-11-10 14:13:44 +0300

    Slow zoom implemented

--- Camera.gd
+++ Camera.gd
@@ -1,46 +1,50 @@
 extends Camera2D
 
-@onready var zoom_tween = create_tween()
 
-var min_zoom = 0.1
-var max_zoom = 0.75
+var min_zoom = 0.5
+var max_zoom = 2.0
 var zoom_step = 0.05
 var zoom_acceleration = 0.005
-var zoom_speed = 0
+var zoom_speed = 0.0
 var zoom_threshold = 0.05
-var camera_speed = 20
+var zoom_sliding = 0.005 / 50
+var camera_speed = 10
 
 var input_direction = Vector2.ZERO
 
 
 func _process(delta):
+	_move_camera()
+	_zoom_camera()
+
+func _move_camera():
 	_get_input_direction()
 	position += input_direction * camera_speed
-	if zoom.x < (max_zoom-zoom_threshold) and zoom.x > (min_zoom+zoom_threshold):
-		if zoom_speed > 0:
-			zoom.x += zoom_speed
-			zoom.y += zoom_speed
-		elif zoom_speed < 0:
-			zoom.x -= zoom_speed
-			zoom.y -= zoom_speed
-	elif zoom.x >= (max_zoom-zoom_threshold):
+
+func _zoom_camera():
+	if zoom.x < (max_zoom - zoom_threshold) and zoom.x > (min_zoom + zoom_threshold):
+		zoom.x += zoom_speed
+		zoom.y += zoom_speed
+	elif zoom.x >= (max_zoom - zoom_threshold):
 		if zoom_speed > 0 and zoom.x < max_zoom:
 			zoom_speed = 0
-			print("here")
-			zoom_tween.tween_property(self, "zoom", Vector2(max_zoom, max_zoom), 1)
-			await get_tree().create_timer(1).timeout
+			print("here 2")
+			zoom.move_toward(Vector2(max_zoom, max_zoom), zoom_step)
 		elif zoom_speed < 0:
-			zoom.x -= zoom_speed
-			zoom.y -= zoom_speed
-	elif zoom.x <= (min_zoom+zoom_threshold):
-		if zoom_speed < 0 and zoom.x > max_zoom:
+			zoom.x += zoom_speed
+			zoom.y += zoom_speed
+	elif zoom.x <= (min_zoom + zoom_threshold):
+		if zoom_speed < 0 and zoom.x > min_zoom:
 			zoom_speed = 0
-			print("here")
-			zoom_tween.tween_property(self, "zoom", Vector2(min_zoom, min_zoom), 1)
-			await get_tree().create_timer(1).timeout
+			print("here 3")
+			zoom.move_toward(Vector2(min_zoom, min_zoom), zoom_step)
 		elif zoom_speed > 0:
 			zoom.x += zoom_speed
 			zoom.y += zoom_speed
+	if zoom_speed > 0:
+		zoom_speed -= zoom_sliding
+	elif zoom_speed < 0:
+		zoom_speed += zoom_sliding
 
 func _input(event):
 	if event is InputEventMouseButton:
